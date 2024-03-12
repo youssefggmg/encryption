c9c7f7f8ed58a45510cabd4081800ef196e3c084
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-03-12 10:35:28 +0000

    half done

--- encryption .c
+++ encryption .c
@@ -2,7 +2,19 @@
 
 int main () {
     char userpass[]={};
+
     printf("enter your passwored to be encryped");
+
+    scanf("%s",userpass);
+
+    int lenght=sizeof(userpass)/sizeof(userpass[0]);
+
+
+    char passAfterEcn[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        passAfterEcn[i]=userpass[i]+3;
+    }
     
     return 0;
 }
